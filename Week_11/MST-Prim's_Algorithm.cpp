15ac564dba17249db00a34ee45bd17876a871753
Patric Nilackshan <126662734+patricnilackshan@users.noreply.github.com>
2023-06-02 09:51:01 +0530

    MST-Prim's_Algorithm

---
+++ Week_11/MST-Prim's_Algorithm.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define V 6
+
+int minKey(int key[], bool mstSet[])
+{
+    int min = INT_MAX, min_index;
+  
+    for (int v = 0; v < V; v++)
+        if (mstSet[v] == false && key[v] < min)
+            min = key[v], min_index = v;
+  
+    return min_index;
+}
+
+void printMST(int parent[], vector<vector<int>> graph)
+{
+    cout<<"Edge \tWeight\n";
+    for (int i = 1; i < V; i++)
+        cout<<parent[i]<<" - "<<i<<" \t"<<graph[i][parent[i]]<<" \n";
+}
+
+void primMST(vector<vector<int>> graph)
+{
+    int parent[V];
+    int key[V];
+    bool mstSet[V];
+
+    for (int i = 0; i < V; i++)
+        key[i] = INT_MAX, mstSet[i] = false;
+
+    key[0] = 0;   
+    parent[0] = -1; 
+
+    for (int count = 0; count < V-1; count++)
+    {
+        int u = minKey(key, mstSet);
+
+        mstSet[u] = true;
+
+        for (int v = 0; v < V; v++)
+            if (graph[u][v] && mstSet[v] == false && graph[u][v] <  key[v])
+                parent[v]  = u, key[v] = graph[u][v];
+    }
+
+    printMST(parent, graph);
+}
+
+int main()
+{
+    vector<vector<int>> graph = {{0,3,0,0,0,1},{3,0,2,1,10,0},{0,2,0,3,0,5},{0,1,3,0,5,0},{0,10,0,5,0,4},{1,0,5,0,4,0}};
+
+    primMST(graph);
+
+    return 0;
+}
